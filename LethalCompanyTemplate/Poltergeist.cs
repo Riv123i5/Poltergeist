07c8a112ee3df42faab249e04b72c765d9c2ec99
coderCleric <wescamphouse@gmail.com>
2024-03-29 15:16:30 -0600

    Revert "Basics of csync set up."
    This reverts commit 03147acea9f1b7d2bbe5983ee6cfd8b9add1b455.

--- LethalCompanyTemplate/Poltergeist.cs
+++ LethalCompanyTemplate/Poltergeist.cs
@@ -8,14 +8,21 @@ namespace Poltergeist
     public class Poltergeist : BaseUnityPlugin
     {
         private static Poltergeist instance = null;
-        public static PoltergeistConfig config { get; private set; }
 
         private void Awake()
         {
             instance = this;
 
-            //Initialize the config
-            config = new PoltergeistConfig(base.Config);
+            //Handle the config
+            Patches.defaultMode = Config.Bind<bool>("General",
+                "DefaultToVanilla",
+                false,
+                "If true, the vanilla spectate system will be used by default on death.").Value;
+            SpectatorCamController.lightIntensity = Config.Bind<float>("General",
+                "GhostLightIntensity",
+                5,
+                "The intensity of the global light when dead.\n" +
+                "WARNING: This game has a lot of fog, so excessively high values can decrease visibility.").Value;
 
             //Make the patches
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
